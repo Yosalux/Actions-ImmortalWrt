883921ea5ba264b12722d2df4c762bc18e20a9de
Yi Shenao <88373432+Yosalux@users.noreply.github.com>
2025-05-25 18:36:02 +0800

    turbo-acc

--- diy.sh
+++ diy.sh
@@ -2,3 +2,5 @@
 # Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
+# turbo-acc插件的安装
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
